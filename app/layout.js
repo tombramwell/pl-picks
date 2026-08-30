cb5769dad98f30f688d5474f72ca8e176e8c0cb9
tombramwell <tom.bramwell@gmail.com>
2026-08-30 12:45:09 +0100

    session error

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import OneSignalInit from '@/components/OneSignalInit';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,10 +12,16 @@ export const metadata = {
 };
 
 
-export default function RootLayout({ children }) {
+// 2. Make sure the function is async
+export default async function RootLayout({ children }) {
+  
+  // 3. Fetch the active user session
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 min-h-screen`}>
+        {/* Now it safely knows what session is! */}
         <OneSignalInit userEmail={session?.user?.email} />
         {children}
       </body>
